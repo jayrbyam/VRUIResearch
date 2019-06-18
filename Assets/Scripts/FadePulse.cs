eb08f761d9e238f93c9d9927822374b9017c819e
Jay Byam <jay.r.byam@gmail.com>
2019-06-17 16:05:32 -0800

    Completed starting screen and started structure for scene swapping

---
+++ Assets/Scripts/FadePulse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FadePulse : MonoBehaviour
+{
+    private bool hide;
+    public bool Hide {
+        set
+        {
+            hide = value;
+            fadingOut = value;
+        }
+        get { return hide; }
+    }
+    private bool fadingOut = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color oldColor = GetComponent<TextMeshPro>().color;
+        if (oldColor.a <= 0f)
+        {
+            fadingOut = false;
+            if (hide)
+            {
+                transform.parent.gameObject.SetActive(false);
+                MainController.Instance.SetSceneIdx(1);
+            }
+        }
+        if (oldColor.a >= 1f) fadingOut = true;
+        GetComponent<TextMeshPro>().color = new Vector4(oldColor.r, oldColor.g, oldColor.b, oldColor.a + (fadingOut ? -0.005f : 0.005f));
+    }
+}
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MainController : MonoBehaviour
+{
+    // Singleton pattern
+    private static MainController _instance;
+    public static MainController Instance { get { return _instance; } }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public List<GameObject> scenes;
+
+    // Indeces for "scenes" in the experience
+    // 0 - Start screen
+    // 1 - Pre-experience questions
+    private int sceneIdx = 0;
+
+    // Scene 0
+    public FadePulse startText;
+
+    // Scene 1
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject child in scenes)
+        {
+            child.SetActive(false);
+        }
+        scenes[sceneIdx].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (sceneIdx)
+        {
+            case 0:
+                if (SteamVR_Actions.default_InteractUI[SteamVR_Input_Sources.Any].state)
+                {
+                    startText.Hide = true;
+                }
+                break;
+            case 1:
+
+                break;
+        }
+        
+    }
+
+    public void SetSceneIdx (int idx)
+    {
+        scenes[sceneIdx].SetActive(false);
+        sceneIdx = idx;
+        scenes[sceneIdx].SetActive(true);
+    }
+}
